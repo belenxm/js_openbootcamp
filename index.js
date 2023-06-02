e335475c9bf9621fb677ebceac3c73339c678409
fer.belen <belenxmorales@gmail.com>
2023-06-02 12:51:47 -0300

    lista de elementos javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+const misdatos = {
+    nombre: 'Fernanda Morales',
+    edad: 31,
+    isWebDeveloper: 'true',
+    date: '20/7/1991',
+    favouriteBook: {
+        nombre: 'Que harias si no tuvieras miedo',
+        autor:'Borja vilaseca',
+        fecha:2020,
+        url:'https://www.borjavilaseca.com.ar/quehariassinotuvierasmiedo'
+    }
+};
+
